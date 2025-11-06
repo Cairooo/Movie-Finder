5c72778391bc1e7a71bfa2ed8824ed887945ad78
Cairo <cairo.92@hotmail.com>
2025-11-06 16:42:31 -0300

    Complete Movie Finder web application

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://www.omdbapi.com/?apikey=9772a00&t=";
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "null" });
+});
+
+app.post("/find", async (req, res) => {
+  try {
+    const movieName = encodeURIComponent(req.body.movieName.trim());
+    const result = await axios.get(API_URL + movieName);
+    res.render("index2.ejs", {
+      content: JSON.stringify(result.data, null, 2),
+    });
+  } catch (error) {
+    res.status(500);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
